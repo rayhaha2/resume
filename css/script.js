0fbee5f88ea7040e1655705ff9dab78b0abe9267
yourname <youremail>
2016-02-22 16:42:09 +0800

    add1

--- css/script.js
+++
@@ -1,9 +0,0 @@
-//页面加载后触发
-window.onload = function(){
-    var btn = document.getElementById('backTop');
-    
-    btn.onclick = function(){
-        var osTop = document.documentElement.scrollTop;
-        alert(osTop);
-    }
-}
--- js/script1.js
+++
@@ -1,7 +0,0 @@
-//页面加载后触发
-window.onload = function(){
-    var btn1 = document.getElementById('kkk');
-    btn1.onclick = function(){
-        alert("hellworld");
-    }
-}
